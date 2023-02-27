c425aefa48c5c6ba224448f0b5141a858721ea56
KawaiiPantsu <12233528+kawaiipantsu@users.noreply.github.com>
2023-02-27 17:16:05 +0100

    new wss js file

---
+++ static/js/websocket-fqdn.js
@@ -0,0 +1,11 @@
+//========================================================
+// CHANGE THE WEBSOCKET ENDPOINT :)
+// Remember to add alternative PORT if you dont use
+// default HTTPS 443 - Must be the FQDN of the certificate
+
+const wssWebSocket = 'wss://mydomain.tld/'
+
+
+
+
+//========================================================
--- static/js/websocket.js
+++ static/js/websocket.js
@@ -1,15 +1,3 @@
-//========================================================
-// CHANGE THE WEBSOCKET ENDPOINT :)
-// Remember to add alternative PORT if you dont use
-// default HTTPS 443 - Must be the FQDN of the certificate
-
-const wssWebSocket = 'wss://mydomain.tld/'
-
-
-
-
-//========================================================
-
 var ws = null;
 var wsRunning = false;
 var wsProblem = false;
